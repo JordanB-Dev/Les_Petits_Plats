16a1c25b7146d89e7314bc43ed1d0676de44a424
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-08-06 09:50:01 +0200

    recipes DOM

--- js/templates/recipes.js
+++ js/templates/recipes.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 const recipeTemplate = (data) => {
-  const { image, name, ingredients, time, description, appliance, ustensils } =
-    data
+  const { image, name, ingredients, time, description } = data
   const picture = `assets/images/${image}`
 
   const getRecipeCardDOM = () => {
@@ -12,6 +11,11 @@ const recipeTemplate = (data) => {
     const h4 = document.createElement('h4')
     const divDesc = document.createElement('div')
     const pDesc = document.createElement('p')
+    const divIngre = document.createElement('div')
+    const h4Ingre = document.createElement('h4')
+    const divContent = document.createElement('div')
+    const divTime = document.createElement('div')
+    const pTime = document.createElement('p')
 
     article.classList.add('recipes_content')
 
@@ -43,6 +47,50 @@ const recipeTemplate = (data) => {
     pDesc.setAttribute('aria-label', `Description recette`)
     article.appendChild(pDesc)
     divDesc.appendChild(pDesc)
+
+    divIngre.classList.add('recipes_ingredients')
+    article.append(divIngre)
+
+    h4Ingre.textContent = 'INGREDIENTS'
+    h4Ingre.setAttribute('aria-label', `Titre ingredients`)
+    article.appendChild(h4Ingre)
+    divIngre.appendChild(h4Ingre)
+
+    divContent.classList.add('recipes_ingredients-content')
+    article.append(divContent)
+
+    ingredients.forEach((ingredient) => {
+      const divContent2 = document.createElement('div')
+      const pName = document.createElement('p')
+      const pQuantity = document.createElement('p')
+
+      divContent2.classList.add('recipes_ingredient-content')
+      article.appendChild(divContent2)
+      divContent.appendChild(divContent2)
+
+      pName.textContent = ingredient.ingredient
+      pName.classList.add('recipes_ingredient-name')
+      pName.setAttribute('aria-label', `Ingrédient`)
+      article.appendChild(pName)
+      divContent2.appendChild(pName)
+
+      pQuantity.innerText = `${ingredient.quantity || '-'} ${
+        ingredient.unit || ''
+      }`
+      pQuantity.classList.add('recipes_ingredient-quantity')
+      pQuantity.setAttribute('aria-label', `Ingrédient`)
+      article.appendChild(pQuantity)
+      divContent2.appendChild(pQuantity)
+    })
+
+    divTime.classList.add('recipes_time')
+    article.append(divTime)
+
+    pTime.innerText = `${time}min`
+    pTime.classList.add('recipes_time-text')
+    pTime.setAttribute('aria-label', `Temps`)
+    article.appendChild(pTime)
+    divTime.appendChild(pTime)
 
     return article
   }
